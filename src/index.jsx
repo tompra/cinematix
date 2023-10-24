b08b3f36b0966868d20974de939c4234451dbefb
tompra <tomprayon@gmail.com>
2023-10-24 11:22:36 +0200

    Fix bootstrap import

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
