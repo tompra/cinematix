38b9de98902eb8662e4b1ee5c74f47c7d64fbdc4
tompra <tomprayon@gmail.com>
2023-11-12 13:04:36 +0100

    Add loading context

--- src/components/routes/movie-route/movie-route.jsx
+++ src/components/routes/movie-route/movie-route.jsx
@@ -2,8 +2,10 @@ import { SpinnerComp } from '../../shared/spinner/spinner';
 import { MovieView } from '../../views/movie-view/movie-view';
 import { NavBar } from '../../shared/nav-bar/nav-bar';
 import { AuthenticatedRoute } from '../authenticated-route/authenticated-route';
+import { useAuthCtx } from '../../../context/auth-context';
 
 export function MovieRoute() {
+    const { loading } = useAuthCtx();
     return (
         <AuthenticatedRoute>
             {loading ? (
